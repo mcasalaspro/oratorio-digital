90893d821879d038e4a42276fce4a1b625e67f78
Maurizio <Maurizio.casalaspro@gmail.com>
2026-06-21 11:10:17 +0200

    fix config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 import { defineConfig } from 'astro/config';
-// ───────────────────────────────────────────────────────────────
-//  CONFIGURAÇÃO DO DEPLOY (Domínio customizado)
-//
-//  Usando domínio próprio: www.oratoriodigital.com
-// ───────────────────────────────────────────────────────────────
+
 export default defineConfig({
   site: 'https://www.oratoriodigital.com',
   base: '/',
-<<<<<<< HEAD
   trailingSlash: 'ignore',
   build: { format: 'directory' },
 });
-=======
-  ...
-})
->>>>>>> fb3f847242b7e2c49376e98571385aeb79c65e48
